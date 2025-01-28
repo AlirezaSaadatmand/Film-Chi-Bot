1674b858674950b3b771835393a8b178f12850c1
AlirezaSaadatmand <alirezasaadat056@gmail.com>
2025-01-28 20:40:19 +0330

    variables

--- utils/variables.php
+++ utils/variables.php
@@ -1,53 +1,7 @@
 <?php
 
-function bot(string $method, array $params) {
-    $ch = curl_init();
-    curl_setopt_array($ch, [
-        CURLOPT_URL => 'https://api.telegram.org/bot' . TOKEN . '/' . $method,
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_POST => true,
-        CURLOPT_POSTFIELDS => $params
-    ]);
-    $result = curl_exec($ch);
-    curl_close($ch);
-    return json_decode($result, true);
-}
-
-function setWebhook() {
-    $result = bot("setWebhook", [
-        "url" => "YOUR WEBHOOK URL",
-        "drop_pending_updates" => true
-    ]);
-    return $result;
-}
-
-function sendMessage($chat_id, $text, $reply_markup = null) {
-
-    $data = [
-        'chat_id' => $chat_id,
-        'text' => $text,
-        'parse_mode' => 'HTML'
-    ];
-    if ($reply_markup != null) {
-        $data['reply_markup'] = $reply_markup;
-    }
-
-    return bot('sendMessage', $data);
-}
-
-function sendChatAction($chat_id, $action) {
-    return bot('sendChatAction', [
-        'chat_id' => $chat_id,
-        'action'  => $action
-    ]);
-}
-
-
-function debug($data, $reply_markup = null) {
-    $result = print_r($data, true);
-    if ($reply_markup == null) {
-        return sendMessage( ADMIN, $result);
-    } else {
-        return sendMessage( ADMIN, $result, $reply_markup);
-    }
+if (array_key_exists('message', $input)) {
+    $firstName = $input['message']['from']['first_name'];
+    $fromId = $input['message']['from']['id'];
+    $text = $input['message']['text'];
 }
