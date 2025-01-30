3841a538be914a4e5d19417b04e969e490dbc7cf
AlirezaSaadatmand <alirezasaadat056@gmail.com>
2025-01-31 00:27:02 +0330

    main file and simple change

--- bot.php
+++ bot.php
@@ -5,10 +5,26 @@ ini_set('display_errors', 1);
 date_default_timezone_set("Asia/Tehran");
 
 
-$input = json_decode(file_get_contents("php://input") , true);
+$input = json_decode(file_get_contents("php://input"), true);
 
 require("utils/variables.php");
 require("functions/botFunctions.php");
 require("functions/requestFunctions.php");
+require("functions/userFunctions.php");
 
-sendMessage($fromId , "hello world");
+$userDataFile = __DIR__ . "/database/users.json";
+
+$step = getStep($chatId, $userDataFile);
+
+
+if (!$step) {
+    addUser($chatId, $username, $userDataFile);
+}
+
+if ($text == "/start") {
+    sendMessage($chatId, $step);
+}
+
+// $response = chatBot($text);
+
+// sendMessage($fromId , $response);
