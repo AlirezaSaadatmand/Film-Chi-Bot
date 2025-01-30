f85d62b13beff5e1f083ebd387d17878b1e91f8e
AlirezaSaadatmand <alirezasaadat056@gmail.com>
2025-01-31 00:24:27 +0330

    add user function

--- functions/userFunctions.php
+++ functions/userFunctions.php
@@ -17,3 +17,28 @@ function getStep($chatId, $filename)
 
     return null;
 }
+
+
+function addUser($chatId, $username, $filename)
+{
+    $defaultStep = 'home';
+
+    if (!file_exists($filename)) {
+        $data = ["users" => []];
+    } else {
+        $jsonData = file_get_contents($filename);
+        $data = json_decode($jsonData, true);
+    }
+
+    $newId = count($data['users']) > 0 ? max(array_column($data['users'], 'id')) + 1 : 1;
+
+    $data['users'][] = [
+        "id" => $newId,
+        "cahtid" => $chatId,
+        "username" => $username,
+        "step" => $defaultStep
+    ];
+
+    file_put_contents($filename, json_encode($data, JSON_PRETTY_PRINT));
+    return true;
+}
